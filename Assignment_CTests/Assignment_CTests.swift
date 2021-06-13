28ed21b6e8566894e0356ca14104baa975c36da9
mmt8618 <Akhil.Jain@go-mmt.com>
2021-06-13 11:52:29 +0530

    Initial Commit

---
+++ Assignment_CTests/Assignment_CTests.swift
@@ -0,0 +1,33 @@
+//
+//  Assignment_CTests.swift
+//  Assignment_CTests
+//
+//  Created by Akhil on 13/06/21.
+//
+
+import XCTest
+@testable import Assignment_C
+
+class Assignment_CTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
